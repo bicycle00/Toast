aa40b9e66179615ba491e7be490f78fdaab2552b
Uiseung Hwang <uiseung.hwang@gmail.com>
2022-03-13 01:39:10 -0800

    Replace !!! to ...

--- app/src/main/java/com/cocoaenglish/toast/MainActivity.java
+++ app/src/main/java/com/cocoaenglish/toast/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textViewMessage = findViewById(R.id.textView);
-        textViewMessage.setText("Good night!!!");
+        textViewMessage.setText("Good night...");
     }
 }
